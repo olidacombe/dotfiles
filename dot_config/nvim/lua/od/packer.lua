10d69fd03e9373cc12eb0740a7b48829a8d86415
Oli Dacombe <odacombe@blockchain.com>
2022-12-18 20:26:57 +0200

    undotree omg

--- dot_config/nvim/lua/od/packer.lua
+++ dot_config/nvim/lua/od/packer.lua
@@ -1,15 +1,11 @@
 vim.cmd.packadd("packer.nvim")
 
 return require('packer').startup(function(use)
+  -- Harpoon
+  use('theprimeagen/harpoon')
+
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
-
-  -- Telescope for fuzzy finding
-  use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
-	  -- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-  }
 
   -- Rose Pine colour scheme
   use({
@@ -20,11 +16,18 @@ return require('packer').startup(function(use)
 	  end
   })
 
+  -- Telescope for fuzzy finding
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  -- or                            , branch = '0.1.x',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
   -- Treesitter
   use('nvim-treesitter/nvim-treesitter', {run = 'TSUpdate'})
   use('nvim-treesitter/playground')
 
-  -- Harpoon
-  use('theprimeagen/harpoon')
+  -- Undotree
+  use('mbbill/undotree')
 
 end)
