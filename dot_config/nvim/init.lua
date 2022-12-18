be9be0317319ba9e1f077d9f60de120d137c8e9c
Oli Dacombe <odacombe@blockchain.com>
2022-12-18 17:13:06 +0200

    a colour scheme

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,3 +1,5 @@
 require("od")
 
-vim.cmd "autocmd BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path \"%\""
+-- Automatically apply changes when editing chezmoi-controlled files
+-- TODO remove hardcode of "~/.local/share/chezmoi/*"?
+vim.cmd.autocmd("BufWritePost ~/.local/share/chezmoi/* ! chezmoi apply --source-path \"%\"")
--- dot_config/nvim/lua/od/packer.lua
+++ dot_config/nvim/lua/od/packer.lua
@@ -4,10 +4,20 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
 
+  -- Telescope for fuzzy finding
   use {
 	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
 	  -- or                            , branch = '0.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   }
+
+  -- Rose Pine colour scheme
+  use({
+	  'rose-pine/neovim',
+	  as = 'rose-pine',
+	  config = function()
+		  vim.cmd('colorscheme rose-pine')
+	  end
+  })
 
 end)
