98663d6917f28141723be9dc0867631ebda218b0
Oli Dacombe <odacombe@blockchain.com>
2022-12-19 00:21:53 +0200

    Quick git push mapping

--- dot_config/nvim/after/plugin/fugitive.lua
+++ dot_config/nvim/after/plugin/fugitive.lua
@@ -1,1 +1,6 @@
+-- Git status
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+-- Quick push (TODO use an existing tool better?)
+vim.keymap.set("n", "<leader>gp", function () vim.cmd.Git("push origin HEAD") end)
+
