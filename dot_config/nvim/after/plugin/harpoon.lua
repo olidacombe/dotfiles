79f011e41cbf1e493aca8e49661e0b2e409f4f6b
Oli Dacombe <odacombe@blockchain.com>
2022-12-29 15:49:36 +0000

    Update hapoon mappings for 0-indexed marks

--- dot_config/nvim/after/plugin/harpoon.lua
+++ dot_config/nvim/after/plugin/harpoon.lua
@@ -1,7 +1,7 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-register_normal = require("od.which-key").register_normal
+local register_normal = require("od.which-key").register_normal
 
 local mappings = {
     a = { mark.add_file, "Harpoon Mark" },
@@ -19,15 +19,15 @@ local mappings = {
         end)
     end, "Execute in Terminal" },
 }
-mappings["1"] = { function() ui.nav_file(1) end, "(Harpoon)" }
-mappings["2"] = { function() ui.nav_file(2) end, "(Harpoon)" }
-mappings["3"] = { function() ui.nav_file(3) end, "(Harpoon)" }
-mappings["4"] = { function() ui.nav_file(4) end, "(Harpoon)" }
-mappings["5"] = { function() ui.nav_file(5) end, "(Harpoon)" }
-mappings["6"] = { function() ui.nav_file(6) end, "(Harpoon)" }
-mappings["7"] = { function() ui.nav_file(7) end, "(Harpoon)" }
-mappings["8"] = { function() ui.nav_file(8) end, "(Harpoon)" }
-mappings["9"] = { function() ui.nav_file(9) end, "(Harpoon)" }
+mappings["0"] = { function() ui.nav_file(1) end, "(Harpoon)" }
+mappings["1"] = { function() ui.nav_file(2) end, "(Harpoon)" }
+mappings["2"] = { function() ui.nav_file(3) end, "(Harpoon)" }
+mappings["3"] = { function() ui.nav_file(4) end, "(Harpoon)" }
+mappings["4"] = { function() ui.nav_file(5) end, "(Harpoon)" }
+mappings["5"] = { function() ui.nav_file(6) end, "(Harpoon)" }
+mappings["6"] = { function() ui.nav_file(7) end, "(Harpoon)" }
+mappings["7"] = { function() ui.nav_file(8) end, "(Harpoon)" }
+mappings["8"] = { function() ui.nav_file(9) end, "(Harpoon)" }
 
 register_normal(mappings)
 
