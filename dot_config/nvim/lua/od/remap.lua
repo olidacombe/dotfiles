8b681d3bc7207ebfd61fc7f614fb98ef595ab14c
Oli Dacombe <odacombe@blockchain.com>
2023-01-15 22:48:55 +0000

    Some fold remaps and a statuscolumn experiment which I backed out because nvim master was giving me unrelated errors which I've no time now to understand

--- dot_config/nvim/lua/od/remap.lua
+++ dot_config/nvim/lua/od/remap.lua
@@ -8,4 +8,14 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<Tab>", ":tabn<CR>")
 vim.keymap.set("n", "<S-Tab>", ":tabp<CR>")
 
+-- Fold Nav
+vim.keymap.set("n", "<Left>", "zc")
+vim.keymap.set("n", "<Right>", "zo")
+vim.keymap.set("n", "<leader><Left>", "zC")
+vim.keymap.set("n", "<leader><Right>", "zO")
+vim.keymap.set("n", "<Up>", "zk")
+vim.keymap.set("n", "<Down>", "zj")
+-- vim.keymap.set("n", "<S-Up>", "[z")
+-- vim.keymap.set("n", "<S-Down>", "]z")
+
 -- All else has moved to `which-key.lua`
