8ac30216ab26b9b7f02f0b9a5dc6fe43976d5649
Oli Dacombe <odacombe@blockchain.com>
2023-01-24 22:38:58 +0000

    Harpoon speed switches :D

--- dot_config/nvim/after/plugin/harpoon.lua
+++ dot_config/nvim/after/plugin/harpoon.lua
@@ -19,17 +19,14 @@ local mappings = {
         end)
     end, "Execute in Terminal" },
 }
-mappings["0"] = { function() ui.nav_file(1) end, "(Harpoon)" }
-mappings["1"] = { function() ui.nav_file(2) end, "(Harpoon)" }
-mappings["2"] = { function() ui.nav_file(3) end, "(Harpoon)" }
-mappings["3"] = { function() ui.nav_file(4) end, "(Harpoon)" }
-mappings["4"] = { function() ui.nav_file(5) end, "(Harpoon)" }
-mappings["5"] = { function() ui.nav_file(6) end, "(Harpoon)" }
-mappings["6"] = { function() ui.nav_file(7) end, "(Harpoon)" }
-mappings["7"] = { function() ui.nav_file(8) end, "(Harpoon)" }
-mappings["8"] = { function() ui.nav_file(9) end, "(Harpoon)" }
 
 register_normal(mappings)
+
+-- easy quick-switches :D
+vim.keymap.set("n", "æ", function() ui.nav_file(1) end)
+vim.keymap.set("n", "≤", function() ui.nav_file(2) end)
+vim.keymap.set("n", "≥", function() ui.nav_file(3) end)
+vim.keymap.set("n", "π", function() ui.nav_file(4) end)
 
 -- Switch back from terminal
 vim.keymap.set("t", "<C-x>", "<C-\\><C-n>:e #<CR>")
