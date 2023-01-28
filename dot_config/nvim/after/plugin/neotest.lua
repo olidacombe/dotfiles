7c876f38ae8730ad91dfddbff83d526ca8a88b4b
Oli Dacombe <odacombe@blockchain.com>
2023-01-28 00:37:54 +0000

    Neotest mappings

--- dot_config/nvim/after/plugin/neotest.lua
+++ dot_config/nvim/after/plugin/neotest.lua
@@ -1,3 +1,5 @@
+local register_normal = require("od.which-key").register_normal
+
 require("neotest").setup({
     adapters = {
         require("neotest-python")({
@@ -10,5 +12,22 @@ require("neotest").setup({
         require("neotest-vim-test")({
             ignore_file_types = { "lua", "python", "rust", "vim", },
         }),
+    },
+})
+
+register_normal({
+    t = {
+        name = "Test / Tab",
+        a = { "<cmd>lua require('neotest').run.attach()<cr>", "Attach" },
+        c = { "<cmd>tabc<CR>", "Tab Close" },
+        f = { "<cmd>lua require('neotest').run.run(vim.fn.expand('%'))<cr>", "Run File" },
+        F = { "<cmd>lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>", "Debug File" },
+        l = { "<cmd>lua require('neotest').run.run_last()<cr>", "Run Last" },
+        L = { "<cmd>lua require('neotest').run.run_last({ strategy = 'dap' })<cr>", "Debug Last" },
+        n = { "<cmd>lua require('neotest').run.run()<cr>", "Run Nearest" },
+        N = { "<cmd>lua require('neotest').run.run({strategy = 'dap'})<cr>", "Debug Nearest" },
+        o = { "<cmd>lua require('neotest').output.open({ enter = true })<cr>", "Output" },
+        S = { "<cmd>lua require('neotest').run.stop()<cr>", "Stop" },
+        s = { "<cmd>lua require('neotest').summary.toggle()<cr>", "Summary" },
     },
 })
