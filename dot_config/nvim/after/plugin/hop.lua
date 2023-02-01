531925aa5d492fd49d149103901e490e6c851f05
Oli Dacombe <odacombe@blockchain.com>
2023-02-01 22:52:17 +0000

    unbreak hop.lua

--- dot_config/nvim/after/plugin/hop.lua
+++ dot_config/nvim/after/plugin/hop.lua
@@ -1,3 +1,22 @@
+--  ____________________
+-- <  see :h hop-config >
+--  --------------------
+--   \     .    _  .
+--    \    |\_|/__/|
+--        / / \/ \  \
+--       /__|O||O|__ \
+--      |/_ \_/\_/ _\ |
+--      | | (____) | ||
+--      \/\___/\__/  //
+--      (_/         ||
+--       |          ||
+--       |          ||\
+--        \        //_/
+--         \______//
+--        __ || __||
+--       (____(____)
+require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+
 register_nv = require("od.which-key").register_nv
 
 local mappings = {
--- dot_config/nvim/lua/od/packer.lua
+++ dot_config/nvim/lua/od/packer.lua
@@ -104,10 +104,6 @@ return require('packer').startup(function(use)
     use {
         'phaazon/hop.nvim',
         branch = 'v2',
-        config = function()
-            -- you can configure Hop the way you like here; see :h hop-config
-            require 'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
-        end
     }
 
     -- LSP status UI
