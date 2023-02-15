9c71de8eee528be18a4bca7b5b434ce81281cf7e
Oli Dacombe <odacombe@blockchain.com>
2023-02-15 23:35:09 +0000

    Some colourscheme rationalization attempt

--- dot_config/nvim/after/plugin/colours.lua
+++ dot_config/nvim/after/plugin/colours.lua
@@ -1,15 +1,12 @@
-require("rose-pine").setup({
-    -- covered globally below
-    -- disable_background = true,
-    -- disable_float_background = true,
+local Od_ColourScheme = vim.api.nvim_create_augroup("Od_ColourScheme", {})
+
+vim.api.nvim_create_autocmd("ColorScheme", {
+    group = Od_ColourScheme,
+    pattern = "*",
+    callback = function()
+        local colourscheme = vim.fn.expand("<amatch>")
+        if colourscheme == "gruvbox" then return end
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none", })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+    end
 })
-
-function SetColourScheme(colour)
-    colour = colour or "kanagawa"
-    vim.cmd.colorscheme(colour)
-
-    vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-    vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-end
-
-SetColourScheme()
--- dot_config/nvim/lua/od/lazy.lua
+++ dot_config/nvim/lua/od/lazy.lua
@@ -91,7 +91,17 @@ local plugins = {
     'ellisonleao/gruvbox.nvim',
 
     -- Kanagawa Colourscheme
-    'rebelot/kanagawa.nvim',
+    {
+        'rebelot/kanagawa.nvim',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            -- load the colorscheme here
+            vim.cmd.colorscheme("kanagawa")
+            vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+            vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+        end,
+    },
 
     -- Nordic Colourscheme
     'AlexvZyl/nordic.nvim',
