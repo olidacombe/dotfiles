172eda4c6cceb54340e4950eb672ed75a2d7e7ae
Oli Dacombe <odacombe@blockchain.com>
2023-02-21 15:32:53 +0000

    Clear the #FoldColumn hg background

--- dot_config/nvim/after/plugin/colours.lua
+++ dot_config/nvim/after/plugin/colours.lua
@@ -6,6 +6,7 @@ vim.api.nvim_create_autocmd("ColorScheme", {
     callback = function()
         local colourscheme = vim.fn.expand("<amatch>")
         if colourscheme == "gruvbox" then return end
+        vim.api.nvim_set_hl(0, "FoldColumn", { bg = "none", })
         vim.api.nvim_set_hl(0, "Normal", { bg = "none", })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
