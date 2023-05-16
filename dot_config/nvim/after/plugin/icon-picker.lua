9768cbe8a43c71b0417c1690897d0c66d4258f05
Oli Dacombe <olidacombe@gmail.com>
2023-05-16 21:28:51 +0100

    oh dear

--- dot_config/nvim/after/plugin/icon-picker.lua
+++ dot_config/nvim/after/plugin/icon-picker.lua
@@ -4,14 +4,6 @@ require("icon-picker").setup({
 
 local opts = { noremap = true, silent = true, desc = "icon" }
 
-<<<<<<< HEAD
-vim.keymap.set("n", "<leader>i", "<cmd>IconPickerNormal<cr>", opts)
-vim.keymap.set("i", "<c-i>", "<cmd>IconPickerInsert<cr>", opts)
-||||||| parent of 85e9123 (meh)
-vim.keymap.set("n", "<leader>i", "<cmd>IconPicker<cr>", opts)
-vim.keymap.set("i", "<c-i>", "<cmd>IconPickerInsert<cr>", opts)
-=======
 -- icoN mappings because c-i does some weird tab thing on macos 🤷
 vim.keymap.set("n", "<leader>n", "<cmd>IconPickerNormal<cr>", opts)
 vim.keymap.set("i", "<c-n>", "<cmd>IconPickerInsert<cr>", opts)
->>>>>>> 85e9123 (meh)
