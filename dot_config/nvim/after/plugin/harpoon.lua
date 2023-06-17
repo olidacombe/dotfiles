97fd452e46d42668ad7e3d822d67c264c8f8e7c9
Oli Dacombe <olidacombe@gmail.com>
2023-06-17 23:16:20 +0100

    hopefully these will work on the mac too 🤞

--- dot_config/nvim/after/plugin/harpoon.lua
+++ dot_config/nvim/after/plugin/harpoon.lua
@@ -4,25 +4,25 @@ local ui = require("harpoon.ui")
 local register_normal = require("od.which-key").register_normal
 
 local mappings = {
-	a = { mark.add_file, "Harpoon Mark" },
-	e = { ui.toggle_quick_menu, "Harpoon List" },
-	E = { ":Telescope harpoon marks<CR>", "Harpoon Telescope" },
+    a = { mark.add_file, "Harpoon Mark" },
+    e = { ui.toggle_quick_menu, "Harpoon List" },
+    E = { ":Telescope harpoon marks<CR>", "Harpoon Telescope" },
 }
 
 register_normal(mappings)
 
 -- easy quick-switches :D
-vim.keymap.set("n", "<S-F1>", function()
-	ui.nav_file(1)
+vim.keymap.set("n", "<F13>", function()
+    ui.nav_file(1)
 end)
-vim.keymap.set("n", "<S-F2>", function()
-	ui.nav_file(2)
+vim.keymap.set("n", "<F14>", function()
+    ui.nav_file(2)
 end)
-vim.keymap.set("n", "<S-F3>", function()
-	ui.nav_file(3)
+vim.keymap.set("n", "<F15>", function()
+    ui.nav_file(3)
 end)
-vim.keymap.set("n", "<S-F4>", function()
-	ui.nav_file(4)
+vim.keymap.set("n", "<F16>", function()
+    ui.nav_file(4)
 end)
 
 -- Switch back from terminal
