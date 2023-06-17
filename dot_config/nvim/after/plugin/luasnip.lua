fb8bcd1ed21c1f24c56a66dd10aa62af923b4173
Oli Dacombe <olidacombe@gmail.com>
2023-06-17 23:22:29 +0100

    .phony make target luasnip

--- dot_config/nvim/after/plugin/luasnip.lua
+++ dot_config/nvim/after/plugin/luasnip.lua
@@ -2,7 +2,7 @@ local ls = require("luasnip")
 local extras = require("luasnip.extras")
 local sn = ls.snippet_node
 
-ls.config.set_config({ update_events = 'TextChanged,TextChangedI' })
+ls.config.set_config({ update_events = "TextChanged,TextChangedI" })
 
 -- set snip expansion/jump next key
 vim.keymap.set({ "i", "s" }, "<c-c>", function()
@@ -13,8 +13,8 @@ end, { silent = true })
 
 -- set jump back key
 vim.keymap.set({ "i", "s" }, "<c-g>", function()
-    if ls.jumpable( -1) then
-        ls.jump( -1)
+    if ls.jumpable(-1) then
+        ls.jump(-1)
     end
 end, { silent = true })
 
@@ -32,7 +32,7 @@ vim.keymap.set("n", "<leader><leader>s", function()
     -- TODO, something less "roundabout"?
     vim.cmd.source(vim.api.nvim_get_runtime_file("after/plugin/luasnip.lua", true)[1])
 end, {
-    desc = "Reload Luasnips"
+    desc = "Reload Luasnips",
 })
 
 local c, i, f, s, t = ls.choice_node, ls.insert_node, ls.function_node, ls.s, ls.text_node
@@ -58,15 +58,15 @@ local raycast_preamble = [[
 
 local raycast_preamble_nodes = function()
     return {
-        i(1), -- title
-        c(2, { t "compact", t "silent", t "fullOutput", t "inline" }), -- mode
-        c(3, { t "", sn(nil, { t " ", i(1, "pkg") }) }), -- package name
-        c(4, { t "false", t "true" }), -- needs confirmation
-        c(5, { t "", t '# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }' }), -- argument boilerplate
-        i(6), --description
-        c(7, { t "Oli Dacombe", i(nil, "Author") }),
-        c(8, { t "", sn(nil, { t " https://", i(1, "example.com") }) }), -- author URL
-        i(0)
+        i(1),                                                                                  -- title
+        c(2, { t("compact"), t("silent"), t("fullOutput"), t("inline") }),                     -- mode
+        c(3, { t(""), sn(nil, { t(" "), i(1, "pkg") }) }),                                     -- package name
+        c(4, { t("false"), t("true") }),                                                       -- needs confirmation
+        c(5, { t(""), t('# @raycast.argument1 { "type": "text", "placeholder": "Placeholder" }') }), -- argument boilerplate
+        i(6),                                                                                  --description
+        c(7, { t("Oli Dacombe"), i(nil, "Author") }),
+        c(8, { t(""), sn(nil, { t(" https://"), i(1, "example.com") }) }),                     -- author URL
+        i(0),
     }
 end
 
@@ -76,25 +76,32 @@ local sh_snips = {
     -- check if executable in path
     s("isexe", fmt("command -v {} &> /dev/null {}", { i(1), i(0) })),
     -- raycast script
-    s({ trig = "raycast", docstring = "Raycast Script 󱆃" },
+    s(
+        { trig = "raycast", docstring = "Raycast Script 󱆃" },
         fmt("#!/usr/bin/env bash\n\n" .. raycast_preamble .. "\nset -euo pipefail\n\n{}", raycast_preamble_nodes())
     ),
     -- add dir to path
-    s({ trig = "path", docstring = "Add dir to $PATH" },
-        fmt([[
+    s(
+        { trig = "path", docstring = "Add dir to $PATH" },
+        fmt(
+            [[
                 if [ -d "{}" ]; then
                     export PATH="${{PATH}}:{}"
                 fi
                 {}
-            ]], { i(1), extras.rep(1), i(0) })
+            ]],
+            { i(1), extras.rep(1), i(0) }
+        )
     ),
 }
 
 ls.add_snippets(nil, {
     lua = {
         -- OOP Boilerplate
-        s({ trig = "class", docstring = "Class boilerplate" },
-            fmt([[
+        s(
+            { trig = "class", docstring = "Class boilerplate" },
+            fmt(
+                [[
                 local {} = {{}}
 
                 function {}:new(o)
@@ -103,79 +110,115 @@ ls.add_snippets(nil, {
                     self.__index = self
                     return o
                 end
-            ]], { i(1), extras.rep(1), })
+            ]],
+                { i(1), extras.rep(1) }
+            )
         ),
         -- Module boilerplate
-        s({ trig = "mod", docstring = "Start a module file" },
-            fmt([[
+        s(
+            { trig = "mod", docstring = "Start a module file" },
+            fmt(
+                [[
                 local M = {{}}
 
                 {}
 
                 return M
-            ]], { i(0) })
+            ]],
+                { i(0) }
+            )
         ),
         -- Require with same name
-        s({ trig = "req", docstring = "Require with same name" },
-            fmt('local {} = require("{}")\n{}', { f(function(args)
-                return (args[1][1] or ""):match("[^.]*$") or ""
-            end, { 1 }), i(1), i(0) })
+        s(
+            { trig = "req", docstring = "Require with same name" },
+            fmt(
+                'local {} = require("{}")\n{}',
+                { f(function(args)
+                    return (args[1][1] or ""):match("[^.]*$") or ""
+                end, { 1 }), i(1), i(0) }
+            )
+        ),
+    },
+    make = {
+        -- .PHONY target
+        s(
+            { trig = "phony", docstring = "PHONY target" },
+            fmt(
+                [[
+                .PHONY: {}
+                {}:
+                {}{}
+            ]],
+                { i(1), extras.rep(1), t("\t"), i(0) }
+            )
         ),
     },
     python = {
         -- raycast script
-        s({ trig = "raycast", docstring = "Raycast Script " },
+        s(
+            { trig = "raycast", docstring = "Raycast Script " },
             fmt("#!/usr/bin/env python\n\n" .. raycast_preamble .. "\n\n{}", raycast_preamble_nodes())
-        )
+        ),
     },
     rust = {
         -- Adding test module
         s(
             { trig = "modtest", docstring = "Add test module" },
-            fmt([[
+            fmt(
+                [[
                 #[cfg(test)]
                 mod test {{
                 {}
 
                     {}
                 }}
-            ]], {
-                c(1, { t "    use super::*;", t "" }),
-                i(0),
-            })
+            ]],
+                {
+                    c(1, { t("    use super::*;"), t("") }),
+                    i(0),
+                }
+            )
         ),
         -- Adding test
         s(
             { trig = "test", docstring = "Add test" },
-            fmt([[
+            fmt(
+                [[
                 #[test]
                 fn {}() {}{{
                     {}{}
                 }}
-            ]], {
-                i(1), c(2, {
-                t "",
-                t "-> Result<()> "
-            }), i(0),
-                f(function(arg)
-                    if string.find(arg[1][1], "Result") == nil then
-                        return ""
-                    end
-                    return { "", "    Ok(())" }
-                end, { 2 })
-            }
-            ))
+            ]],
+                {
+                    i(1),
+                    c(2, {
+                        t(""),
+                        t("-> Result<()> "),
+                    }),
+                    i(0),
+                    f(function(arg)
+                        if string.find(arg[1][1], "Result") == nil then
+                            return ""
+                        end
+                        return { "", "    Ok(())" }
+                    end, { 2 }),
+                }
+            )
+        ),
     },
     sh = sh_snips,
     template = {
         s(
             { trig = "osswitch", docstring = "OS-based switch-case" },
-            fmt([[
+            fmt(
+                [[
                 {{{{ if eq .chezmoi.os "darwin" -}}}}
                 {}
                 {{{{ else if eq .chezmoi.os "linux" -}}}}
                 {{{{ end -}}}}
-            ]], {i(0)})
+            ]],
+                { i(0) }
+            )
         ),
     },
     zsh = sh_snips,
