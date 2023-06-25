91b26a6486fd3858121f491bd395a3f1b550723c
Oli Dacombe <olidacombe@gmail.com>
2023-06-25 23:11:18 +0100

    sort things out a bit

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,5 @@
 local lsp = require('lsp-zero')
-local telescope = require("telescope.builtin")
+local on_attach = require("od.lsp").on_attach
 
 lsp.preset('recommended')
 lsp.nvim_workspace()
@@ -64,45 +64,6 @@ lsp.configure('svelte', {
         }
     }
 })
-
-local on_attach = function(client, bufnr)
-    local opts = { buffer = bufnr, remap = false }
-
-    if client.name == "eslint" then
-        client.stop()
-        return
-    end
-
-    -- Try to init the window bar context
-    if client.server_capabilities.documentSymbolProvider then
-        local navic = require "nvim-navic"
-        navic.attach(client, bufnr)
-    end
-
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "gr", telescope.lsp_references, opts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
-    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
-    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
-    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
-
-    local Od_Lsp = vim.api.nvim_create_augroup("Od_Lsp", {})
-
-    local autocmd = vim.api.nvim_create_autocmd
-    -- Format on save
-    autocmd({ "BufWritePre" }, {
-        group = Od_Lsp,
-        pattern = "*",
-        callback = function()
-            vim.lsp.buf.format()
-        end,
-    })
-end
 
 lsp.on_attach(on_attach)
 
---
+++ dot_config/nvim/lua/od/lsp.lua
@@ -0,0 +1,42 @@
+local M = {}
+
+M.on_attach = function(client, bufnr)
+    local opts = { buffer = bufnr, remap = false }
+
+    if client.name == "eslint" then
+        client.stop()
+        return
+    end
+
+    -- Try to init the window bar context
+    if client.server_capabilities.documentSymbolProvider then
+        local navic = require "nvim-navic"
+        navic.attach(client, bufnr)
+    end
+
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
+    vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
+    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
+    vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
+
+    local Od_Lsp = vim.api.nvim_create_augroup("Od_Lsp", {})
+
+    local autocmd = vim.api.nvim_create_autocmd
+    -- Format on save
+    autocmd({ "BufWritePre" }, {
+        group = Od_Lsp,
+        pattern = "*",
+        callback = function()
+            vim.lsp.buf.format()
+        end,
+    })
+end
+
+return M
