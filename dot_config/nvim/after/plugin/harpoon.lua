afa2d3b1fc5c3d8526e238410917ca2cc10f809e
Oli Dacombe <oliver.dacombe@gsr.io>
2023-07-03 14:54:35 +0100

    what on earth is happening today?

--- dot_config/nvim/after/plugin/harpoon.lua
+++ dot_config/nvim/after/plugin/harpoon.lua
@@ -3,6 +3,8 @@ local ui = require("harpoon.ui")
 
 local register_normal = require("od.which-key").register_normal
 
+local os_cb = require("utils").os_cb
+
 local mappings = {
     a = { mark.add_file, "Harpoon Mark" },
     e = { ui.toggle_quick_menu, "Harpoon List" },
@@ -11,19 +13,37 @@ local mappings = {
 
 register_normal(mappings)
 
+os_cb({
+    linux = function()
+        vim.keymap.set("n", "<F13>", function()
+            ui.nav_file(1)
+        end)
+        vim.keymap.set("n", "<F14>", function()
+            ui.nav_file(2)
+        end)
+        vim.keymap.set("n", "<F15>", function()
+            ui.nav_file(3)
+        end)
+        vim.keymap.set("n", "<F16>", function()
+            ui.nav_file(4)
+        end)
+    end,
+    darwin = function()
+        vim.keymap.set("n", "<S-F1>", function()
+            ui.nav_file(1)
+        end)
+        vim.keymap.set("n", "<S-F2>", function()
+            ui.nav_file(2)
+        end)
+        vim.keymap.set("n", "<S-F3>", function()
+            ui.nav_file(3)
+        end)
+        vim.keymap.set("n", "<S-F4>", function()
+            ui.nav_file(4)
+        end)
+    end,
+})
 -- easy quick-switches :D
-vim.keymap.set("n", "<F13>", function()
-    ui.nav_file(1)
-end)
-vim.keymap.set("n", "<F14>", function()
-    ui.nav_file(2)
-end)
-vim.keymap.set("n", "<F15>", function()
-    ui.nav_file(3)
-end)
-vim.keymap.set("n", "<F16>", function()
-    ui.nav_file(4)
-end)
 
 -- Switch back from terminal
 vim.keymap.set("t", "<C-x>", "<C-\\><C-n>:e #<CR>")
