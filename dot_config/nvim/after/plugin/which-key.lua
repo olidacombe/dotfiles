613149ecc336e75dc928a776ac7bc080ab39d8d8
Oli Dacombe <oliver.dacombe@gsr.io>
2023-07-19 12:31:22 +0100

    git blame shorty

--- dot_config/nvim/after/plugin/which-key.lua
+++ dot_config/nvim/after/plugin/which-key.lua
@@ -1,48 +1,54 @@
-local whichkey = require "which-key"
-local wk = require "od.which-key"
+local whichkey = require("which-key")
+local wk = require("od.which-key")
 
 local conf = {
-    window = {
-        border = "single",   -- none, single, double, shadow
-        position = "bottom", -- bottom, top
-    },
+	window = {
+		border = "single", -- none, single, double, shadow
+		position = "bottom", -- bottom, top
+	},
 }
 
 whichkey.setup(conf)
 
-local yank_to_clipboard = { "\"+y", "Yank to Clipboard" }
+local yank_to_clipboard = { '"+y', "Yank to Clipboard" }
 
 -- Normal mode mappings
 local mappings = {
-    b = {
-        name = "Buffer",
-        c = { "<Cmd>bd!<CR>", "Close current buffer" },
-        D = { "<Cmd>%bd|e#|bd#<CR>", "Delete all buffers" },
-    },
-    g = {
-        name = "Git",
-        p = { "<cmd>Git pull<cr>", "﬇ pull" },
-        r = { "<cmd>! gh repo view --web<cr>", "-> Repo" },
-        C = { require('od.git').git_checkout_new_branch, "Create Branch" },
-    },
-    x = { "<cmd>!chmod +x %<CR>", "Make Executable" },
-    -- Yank to "+
-    y = yank_to_clipboard,
-    Y = { "\"+Y", "Yank line to Clipboard" },
+	b = {
+		name = "Buffer",
+		c = { "<Cmd>bd!<CR>", "Close current buffer" },
+		D = { "<Cmd>%bd|e#|bd#<CR>", "Delete all buffers" },
+	},
+	g = {
+		name = "Git",
+		p = { "<cmd>Git pull<cr>", "﬇ pull" },
+		r = { "<cmd>! gh repo view --web<cr>", "-> Repo" },
+		C = { require("od.git").git_checkout_new_branch, "Create Branch" },
+		l = {
+			function()
+				vim.cmd.Git("blame")
+			end,
+			"bLame",
+		},
+	},
+	x = { "<cmd>!chmod +x %<CR>", "Make Executable" },
+	-- Yank to "+
+	y = yank_to_clipboard,
+	Y = { '"+Y', "Yank line to Clipboard" },
 }
 
 mappings["<leader>"] = {
-    g = { ":echo resolve(expand('%:p'))<cr>", "Show full path" }
+	g = { ":echo resolve(expand('%:p'))<cr>", "Show full path" },
 }
 
 wk.register_normal(mappings)
 
 -- Visual Mode mappings
 mappings = {
-    -- Replace selection everywhere
-    r = { 'y:%s/<C-r>"/', "Replace Everywhere" },
-    -- Yank to "+
-    Y = yank_to_clipboard,
+	-- Replace selection everywhere
+	r = { 'y:%s/<C-r>"/', "Replace Everywhere" },
+	-- Yank to "+
+	Y = yank_to_clipboard,
 }
 
 wk.register_visual(mappings)
