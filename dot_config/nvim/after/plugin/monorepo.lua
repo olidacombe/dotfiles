d832117472f2f82357f2456956237afd734f7b55
Oli Dacombe <oliver.dacombe@gsr.io>
2023-08-10 11:19:55 +0100

    enhance monorepo workflow

--- dot_config/nvim/after/plugin/monorepo.lua
+++ dot_config/nvim/after/plugin/monorepo.lua
@@ -1,10 +1,18 @@
 local buffer = require("od.buffer")
 local monorepo = require("monorepo")
 
+local function go_to_project(path)
+	monorepo.go_to_project(require("monorepo.utils").index_of(monorepo.monorepoVars[monorepo.currentMonorepo], path))
+end
+
 vim.keymap.set("n", "<leader>fp", function()
 	require("telescope").extensions.monorepo.monorepo()
 end, { desc = "monorepo project toggle" })
 vim.keymap.set("n", "<leader>p", function()
 	local path = buffer.current_path():sub(monorepo.currentMonorepo:len() + 1):gsub("[^/]*$", "")
-	monorepo.toggle_project(path)
+	monorepo.add_project(path)
+	go_to_project(path)
 end, { desc = "monorepo project" })
+vim.keymap.set("n", "<leader>P", function()
+	go_to_project("/")
+end, { desc = "monorepo root" })
