3dd64a0a02fcd0fc6fc030d6712fff13c7daa451
Oli Dacombe <olidacombe@gmail.com>
2023-11-19 00:01:07 +0000

    start on some basics

--- dot_config/nvim/lua/od/git.lua
+++ dot_config/nvim/lua/od/git.lua
@@ -1,16 +1,18 @@
 local M = {}
 
+M.branch_name = function()
+	return vim.system({ "git", "branch", "--show-current" }, { text = true }):wait().stdout:gsub("[\n\r]", "")
+end
+
 M.git_checkout_new_branch = function()
-    vim.ui.input({
-            prompt = "Branch Name",
-        },
-        function(input)
-            if input then
-                local without_whitespace = input:gsub("%s+", "_")
-                vim.cmd("Git checkout -b " .. without_whitespace)
-            end
-        end
-    )
+	vim.ui.input({
+		prompt = "Branch Name",
+	}, function(input)
+		if input then
+			local without_whitespace = input:gsub("%s+", "_")
+			vim.cmd("Git checkout -b " .. without_whitespace)
+		end
+	end)
 end
 
 return M
