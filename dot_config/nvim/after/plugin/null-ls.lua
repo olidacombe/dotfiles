aabfeb7e9ce5d51c9baa9e4210211672b3914864
Oli Dacombe <olidacombe@gmail.com>
2023-11-20 23:06:08 +0000

    use prettier

--- dot_config/nvim/after/plugin/null-ls.lua
+++ dot_config/nvim/after/plugin/null-ls.lua
@@ -1,9 +1,14 @@
 local null_ls = require("null-ls")
 
 null_ls.setup({
-    sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.completion.spell,
-    },
+	sources = {
+		null_ls.builtins.formatting.stylua,
+		null_ls.builtins.formatting.prettier.with({
+			extra_filetypes = {
+				"svelte",
+			},
+		}),
+		null_ls.builtins.diagnostics.eslint,
+		null_ls.builtins.completion.spell,
+	},
 })
