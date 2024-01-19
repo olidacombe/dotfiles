d4464385da17591ba2b806f64b282a2cfe8469f3
Oli Dacombe <oliver.dacombe@gsr.io>
2024-01-19 18:05:31 +0000

    `tree-sitter-just`

--- dot_config/nvim/lua/od/lazy.lua
+++ dot_config/nvim/lua/od/lazy.lua
@@ -1,331 +1,333 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {
-    dev = {
-        path = "~/od",
-    },
+	dev = {
+		path = "~/od",
+	},
 }
 
 local plugins = {
-    --
-    --  @@@@@@   @@@@@@@
-    -- @@@@@@@@  @@@@@@@@
-    -- @@!  @@@  @@!  @@@
-    -- !@!  @!@  !@!  @!@
-    -- @!@  !@!  @!@  !@!
-    -- !@!  !!!  !@!  !!!
-    -- !!:  !!!  !!:  !!!
-    -- :!:  !:!  :!:  !:!
-    -- ::::: ::   :::: ::
-    --  : :  :   :: :  :
-    --
-    -- Commentalist local dev
-    {
-        "olidacombe/commentalist.nvim",
-        dependencies = {
-            { "numToStr/Comment.nvim" },
-        },
-        dev = true,
-    },
-    -- Makemapper local dev
-    {
-        "olidacombe/makemapper.nvim",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-        dev = true,
-    },
+	--
+	--  @@@@@@   @@@@@@@
+	-- @@@@@@@@  @@@@@@@@
+	-- @@!  @@@  @@!  @@@
+	-- !@!  @!@  !@!  @!@
+	-- @!@  !@!  @!@  !@!
+	-- !@!  !!!  !@!  !!!
+	-- !!:  !!!  !!:  !!!
+	-- :!:  !:!  :!:  !:!
+	-- ::::: ::   :::: ::
+	--  : :  :   :: :  :
+	--
+	-- Commentalist local dev
+	{
+		"olidacombe/commentalist.nvim",
+		dependencies = {
+			{ "numToStr/Comment.nvim" },
+		},
+		dev = true,
+	},
+	-- Makemapper local dev
+	{
+		"olidacombe/makemapper.nvim",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+		dev = true,
+	},
 
-    -- Mini
-    { "echasnovski/mini.nvim", version = false },
+	-- Mini
+	{ "echasnovski/mini.nvim", version = false },
 
-    -- DiffView
-    {
-        "sindrets/diffview.nvim",
-        dependencies = {
-            { "nvim-lua/plenary.nvim" },
-            { "nvim-tree/nvim-web-devicons" },
-        },
-    },
+	-- DiffView
+	{
+		"sindrets/diffview.nvim",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim" },
+			{ "nvim-tree/nvim-web-devicons" },
+		},
+	},
 
-    -- direnv
-    "direnv/direnv.vim",
+	-- direnv
+	"direnv/direnv.vim",
 
-    -- editorconfig.org
-    "editorconfig/editorconfig-vim",
+	-- editorconfig.org
+	"editorconfig/editorconfig-vim",
 
-    -- monorepo
-    {
-        "imNel/monorepo.nvim",
-        config = function()
-            require("monorepo").setup({
-                silent = true,          -- Supresses vim.notify messages
-                autoload_telescope = false, -- do this in after/telescope
-                data_path = vim.fn.stdpath("data"), -- Path that monorepo.json gets saved to
-            })
-        end,
-        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-    },
+	-- tree-sitter-just
+	"IndianBoy42/tree-sitter-just",
 
-    -- Neodev
-    "folke/neodev.nvim",
+	-- monorepo
+	{
+		"imNel/monorepo.nvim",
+		config = function()
+			require("monorepo").setup({
+				silent = true, -- Supresses vim.notify messages
+				autoload_telescope = false, -- do this in after/telescope
+				data_path = vim.fn.stdpath("data"), -- Path that monorepo.json gets saved to
+			})
+		end,
+		dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+	},
 
-    -- Gitlinker
-    {
-        "ruifm/gitlinker.nvim",
-        dependencies = "nvim-lua/plenary.nvim",
-    },
+	-- Neodev
+	"folke/neodev.nvim",
 
-    -- Dressing - pretty up the inputs
-    "stevearc/dressing.nvim",
+	-- Gitlinker
+	{
+		"ruifm/gitlinker.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+	},
 
-    {
-        "stevearc/oil.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
+	-- Dressing - pretty up the inputs
+	"stevearc/dressing.nvim",
 
-    -- ____ ___  ____ _    _ ____ _  _
-    -- |__| |__] |  | |    | [__  |__|
-    -- |  | |__] |__| |___ | ___] |  |
-    --
-    "tpope/vim-abolish",
+	{
+		"stevearc/oil.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 
-    -- Fugitive
-    "tpope/vim-fugitive",
+	-- ____ ___  ____ _    _ ____ _  _
+	-- |__| |__] |  | |    | [__  |__|
+	-- |  | |__] |__| |___ | ___] |  |
+	--
+	"tpope/vim-abolish",
 
-    -- GitSigns
-    "lewis6991/gitsigns.nvim",
+	-- Fugitive
+	"tpope/vim-fugitive",
 
-    -- =======================================================================
-    -- =======================     ==========  ===============================
-    -- ======================  ===  =========  ===============================
-    -- =====================  ===============  ===============================
-    -- =====================  =========   ===  ===   ===  =  ==  =   ====   ==
-    -- =====================  ========     ==  ==     ==  =  ==    =  ==  =  =
-    -- =====================  ========  =  ==  ==  =  ==  =  ==  ========  ===
-    -- =====================  ========  =  ==  ==  =  ==  =  ==  =========  ==
-    -- ======================  ===  ==  =  ==  ==  =  ==  =  ==  =======  =  =
-    -- =======================     ====   ===  ===   ====    ==  ========   ==
-    -- =======================================================================
+	-- GitSigns
+	"lewis6991/gitsigns.nvim",
 
-    -- Catpuccin Colourscheme
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        lazy = false, -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            -- load the colorscheme here
-            vim.cmd.colorscheme("catppuccin")
-            require("od.colours").overrides()
-        end,
-    },
+	-- =======================================================================
+	-- =======================     ==========  ===============================
+	-- ======================  ===  =========  ===============================
+	-- =====================  ===============  ===============================
+	-- =====================  =========   ===  ===   ===  =  ==  =   ====   ==
+	-- =====================  ========     ==  ==     ==  =  ==    =  ==  =  =
+	-- =====================  ========  =  ==  ==  =  ==  =  ==  ========  ===
+	-- =====================  ========  =  ==  ==  =  ==  =  ==  =========  ==
+	-- ======================  ===  ==  =  ==  ==  =  ==  =  ==  =======  =  =
+	-- =======================     ====   ===  ===   ====    ==  ========   ==
+	-- =======================================================================
 
-    -- Gruvbox Colourscheme
-    "ellisonleao/gruvbox.nvim",
+	-- Catpuccin Colourscheme
+	{
+		"catppuccin/nvim",
+		name = "catppuccin",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			-- load the colorscheme here
+			vim.cmd.colorscheme("catppuccin")
+			require("od.colours").overrides()
+		end,
+	},
 
-    -- Kanagawa Colourscheme
-    { "rebelot/kanagawa.nvim" },
+	-- Gruvbox Colourscheme
+	"ellisonleao/gruvbox.nvim",
 
-    -- Nordic Colourscheme
-    {
-        "AlexvZyl/nordic.nvim",
-    },
+	-- Kanagawa Colourscheme
+	{ "rebelot/kanagawa.nvim" },
 
-    -- Neotest
-    "nvim-neotest/neotest-plenary",
-    {
-        "nvim-neotest/neotest",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-treesitter/nvim-treesitter",
-            "antoinemadec/FixCursorHold.nvim",
-            "nvim-neotest/neotest-go",
-            "haydenmeade/neotest-jest",
-            "nvim-neotest/neotest-python",
-            "nvim-neotest/neotest-plenary",
-            -- "rouge8/neotest-rust",
-            -- temp(1) until https://github.com/rouge8/neotest-rust/pull/19
-            { "MarkEmmons/neotest-rust", branch = "feature/dap-support" },
-            "nvim-neotest/neotest-vim-test",
-        },
-    },
+	-- Nordic Colourscheme
+	{
+		"AlexvZyl/nordic.nvim",
+	},
 
-    -- Debugging
-    -- https://alpha2phi.medium.com/neovim-for-beginners-debugging-using-dap-44626a767f57 was handy
-    {
-        "mfussenegger/nvim-dap",
-        -- opt = true,
-        -- event = "BufReadPre",
-        -- module = { "dap" },
-        -- wants = { "nvim-dap-virtual-text", "nvim-dap-ui", "nvim-dap-python", "which-key.nvim" },
-        dependencies = {
-            "theHamsta/nvim-dap-virtual-text",
-            "rcarriga/nvim-dap-ui",
-            -- "mfussenegger/nvim-dap-python",
-            "nvim-telescope/telescope-dap.nvim",
-            -- { "leoluz/nvim-dap-go", module = "dap-go" },
-            -- { "jbyuki/one-small-step-for-vimkind", module = "osv" },
-        },
-    },
+	-- Neotest
+	"nvim-neotest/neotest-plenary",
+	{
+		"nvim-neotest/neotest",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-treesitter/nvim-treesitter",
+			"antoinemadec/FixCursorHold.nvim",
+			"nvim-neotest/neotest-go",
+			"haydenmeade/neotest-jest",
+			"nvim-neotest/neotest-python",
+			"nvim-neotest/neotest-plenary",
+			-- "rouge8/neotest-rust",
+			-- temp(1) until https://github.com/rouge8/neotest-rust/pull/19
+			{ "MarkEmmons/neotest-rust", branch = "feature/dap-support" },
+			"nvim-neotest/neotest-vim-test",
+		},
+	},
 
-    -- Harpoon
-    "theprimeagen/harpoon",
+	-- Debugging
+	-- https://alpha2phi.medium.com/neovim-for-beginners-debugging-using-dap-44626a767f57 was handy
+	{
+		"mfussenegger/nvim-dap",
+		-- opt = true,
+		-- event = "BufReadPre",
+		-- module = { "dap" },
+		-- wants = { "nvim-dap-virtual-text", "nvim-dap-ui", "nvim-dap-python", "which-key.nvim" },
+		dependencies = {
+			"theHamsta/nvim-dap-virtual-text",
+			"rcarriga/nvim-dap-ui",
+			-- "mfussenegger/nvim-dap-python",
+			"nvim-telescope/telescope-dap.nvim",
+			-- { "leoluz/nvim-dap-go", module = "dap-go" },
+			-- { "jbyuki/one-small-step-for-vimkind", module = "osv" },
+		},
+	},
 
-    -- Hop
-    {
-        "phaazon/hop.nvim",
-        branch = "v2",
-    },
+	-- Harpoon
+	"theprimeagen/harpoon",
 
-    -- LSP status UI
-    {
-        "j-hui/fidget.nvim",
-        -- TODO update to main and fix for new options
-        branch = "legacy",
-    },
+	-- Hop
+	{
+		"phaazon/hop.nvim",
+		branch = "v2",
+	},
 
-    -- UFO nice folding
-    { "kevinhwang91/nvim-ufo",           dependencies = "kevinhwang91/promise-async" },
+	-- LSP status UI
+	{
+		"j-hui/fidget.nvim",
+		-- TODO update to main and fix for new options
+		branch = "legacy",
+	},
 
-    -- [LSP](https://github.com/VonHeikemen/lsp-zero.nvim)
-    {
-        "VonHeikemen/lsp-zero.nvim",
-        dependencies = {
-            -- LSP Support
-            { "neovim/nvim-lspconfig" },
-            { "williamboman/mason.nvim" },
-            { "williamboman/mason-lspconfig.nvim" },
+	-- UFO nice folding
+	{ "kevinhwang91/nvim-ufo", dependencies = "kevinhwang91/promise-async" },
 
-            -- Autocompletion
-            { "hrsh7th/nvim-cmp" },
-            { "hrsh7th/cmp-buffer" },
-            { "hrsh7th/cmp-emoji" },
-            { "hrsh7th/cmp-path" },
-            { "saadparwaiz1/cmp_luasnip" },
-            { "hrsh7th/cmp-nvim-lsp" },
-            { "hrsh7th/cmp-nvim-lua" },
+	-- [LSP](https://github.com/VonHeikemen/lsp-zero.nvim)
+	{
+		"VonHeikemen/lsp-zero.nvim",
+		dependencies = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
 
-            -- Snippets
-            { "L3MON4D3/LuaSnip" },
-            { "rafamadriz/friendly-snippets" },
-        },
-    },
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-emoji" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
 
-    -- LUALine
-    {
-        "nvim-lualine/lualine.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-    },
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	},
 
-    --      _       _ _
-    --  ___| |     | | |_   _  __ _
-    -- |__ \ |_____| | | | | |/ _` |
-    -- / __/ |_____| | | |_| | | | |
-    -- \___|_|     |_|_|_.__/|_| |_|
-    --
-    { "jose-elias-alvarez/null-ls.nvim", dependencies = { { "nvim-lua/plenary.nvim" } } },
+	-- LUALine
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+	},
 
-    -- Rose Pine colour scheme
-    {
-        "rose-pine/neovim",
-        as = "rose-pine",
-    },
+	--      _       _ _
+	--  ___| |     | | |_   _  __ _
+	-- |__ \ |_____| | | | | |/ _` |
+	-- / __/ |_____| | | |_| | | | |
+	-- \___|_|     |_|_|_.__/|_| |_|
+	--
+	{ "jose-elias-alvarez/null-ls.nvim", dependencies = { { "nvim-lua/plenary.nvim" } } },
 
-    {
-        "SmiteshP/nvim-navic",
-        dependencies = "neovim/nvim-lspconfig",
-    },
+	-- Rose Pine colour scheme
+	{
+		"rose-pine/neovim",
+		as = "rose-pine",
+	},
 
-    --                  ____   __ __  _____ ______
-    --                 |    \ |  T  T/ ___/|      T
-    --                 |  D  )|  |  (   \_ |      |
-    --                 |    / |  |  |\__  Tl_j  l_j
-    --                 |    \ |  :  |/  \ |  |  |
-    --                 |  .  Yl     |\    |  |  |
-    --                 l__j\_j \__,_j \___j  l__j
-    --
-    --  ______   ___    ___   _      _____
-    -- |      T /   \  /   \ | T    / ___/
-    -- |      |Y     YY     Y| |   (   \_
-    -- l_j  l_j|  O  ||  O  || l___ \__  T
-    --   |  |  |     ||     ||     T/  \ |
-    --   |  |  l     !l     !|     |\    |
-    --   l__j   \___/  \___/ l_____j \___j
-    --
-    {
-        -- "simrat39/rust-tools.nvim", -- until [my PR](https://github.com/simrat39/rust-tools.nvim/pull/431) gets merged..?
-        "olidacombe/rust-tools.nvim",
-        branch = "allow_override_url_opener",
-    },
+	{
+		"SmiteshP/nvim-navic",
+		dependencies = "neovim/nvim-lspconfig",
+	},
 
-    {
-        "saecki/crates.nvim",
-        tag = "v0.3.0",
-        dependencies = { { "nvim-lua/plenary.nvim" } },
-    },
+	--                  ____   __ __  _____ ______
+	--                 |    \ |  T  T/ ___/|      T
+	--                 |  D  )|  |  (   \_ |      |
+	--                 |    / |  |  |\__  Tl_j  l_j
+	--                 |    \ |  :  |/  \ |  |  |
+	--                 |  .  Yl     |\    |  |  |
+	--                 l__j\_j \__,_j \___j  l__j
+	--
+	--  ______   ___    ___   _      _____
+	-- |      T /   \  /   \ | T    / ___/
+	-- |      |Y     YY     Y| |   (   \_
+	-- l_j  l_j|  O  ||  O  || l___ \__  T
+	--   |  |  |     ||     ||     T/  \ |
+	--   |  |  l     !l     !|     |\    |
+	--   l__j   \___/  \___/ l_____j \___j
+	--
+	{
+		-- "simrat39/rust-tools.nvim", -- until [my PR](https://github.com/simrat39/rust-tools.nvim/pull/431) gets merged..?
+		"olidacombe/rust-tools.nvim",
+		branch = "allow_override_url_opener",
+	},
 
-    -- Telescope for fuzzy finding
-    {
-        "nvim-telescope/telescope.nvim", --tag = '0.1.0',
-        -- or                            , branch = '0.1.x',
-        branch = "0.1.x",          -- got me past https://github.com/nvim-telescope/telescope.nvim/issues/2192
-        dependencies = { { "nvim-lua/plenary.nvim" } },
-    },
-    {
-        "nvim-telescope/telescope-fzf-native.nvim",
-        build =
-        "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
-    },
+	{
+		"saecki/crates.nvim",
+		tag = "v0.3.0",
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
 
-    -- Tmux pane navigation integration
-    "christoomey/vim-tmux-navigator",
+	-- Telescope for fuzzy finding
+	{
+		"nvim-telescope/telescope.nvim", --tag = '0.1.0',
+		-- or                            , branch = '0.1.x',
+		branch = "0.1.x", -- got me past https://github.com/nvim-telescope/telescope.nvim/issues/2192
+		dependencies = { { "nvim-lua/plenary.nvim" } },
+	},
+	{
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+	},
 
-    -- Treesitter
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-    },
-    { "nvim-treesitter/playground" },
+	-- Tmux pane navigation integration
+	"christoomey/vim-tmux-navigator",
 
-    -- Undotree
-    "mbbill/undotree",
+	-- Treesitter
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+	},
+	{ "nvim-treesitter/playground" },
 
-    -- nvim-tree
-    {
-        "nvim-tree/nvim-tree.lua",
-        -- dependencies = {
-        --   'nvim-tree/nvim-web-devicons', -- optional, for file icons
-        -- },
-        tag = "nightly", -- optional, updated every week. (see issue #1193)
-    },
+	-- Undotree
+	"mbbill/undotree",
 
-    -- TokyoNight Colourscheme
-    "folke/tokyonight.nvim",
+	-- nvim-tree
+	{
+		"nvim-tree/nvim-tree.lua",
+		-- dependencies = {
+		--   'nvim-tree/nvim-web-devicons', -- optional, for file icons
+		-- },
+		tag = "nightly", -- optional, updated every week. (see issue #1193)
+	},
 
-    -- WhichKey
-    "folke/which-key.nvim",
+	-- TokyoNight Colourscheme
+	"folke/tokyonight.nvim",
 
-    -- wilder.nvim
-    { "gelguy/wilder.nvim",        build = ":UpdateRemotePlugins" },
+	-- WhichKey
+	"folke/which-key.nvim",
 
-    -- Install tools via Mason which aren't LSP servers
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
+	-- wilder.nvim
+	{ "gelguy/wilder.nvim", build = ":UpdateRemotePlugins" },
 
-    -- Icon picker
-    {
-        "ziontee113/icon-picker.nvim",
-        dependencies = { "stevearc/dressing.nvim", "nvim-telescope/telescope.nvim" },
-    },
+	-- Install tools via Mason which aren't LSP servers
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+
+	-- Icon picker
+	{
+		"ziontee113/icon-picker.nvim",
+		dependencies = { "stevearc/dressing.nvim", "nvim-telescope/telescope.nvim" },
+	},
 }
 
 return require("lazy").setup(plugins, opts)
