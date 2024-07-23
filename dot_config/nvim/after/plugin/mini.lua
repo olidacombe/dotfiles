f51489d246bb617021a9ab8e8b83de68d8a9ae0d
Oli Dacombe <oliver.dacombe@gsr.io>
2024-07-23 16:01:51 +0100

    fix mini.surround maps

--- dot_config/nvim/after/plugin/mini.lua
+++ dot_config/nvim/after/plugin/mini.lua
@@ -8,38 +8,38 @@ require("mini.surround").setup()
 -- Surround convenience
 -- see [here](https://github.com/echasnovski/mini.surround/blob/9d1956b576d7051da3a483b251dfc778121c60db/lua/mini/surround.lua#L486-L486)
 require("which-key").add({
-	{ "s", group = "surround", remap = true },
-	{ "sW", "saiW", desc = "Word", remap = true },
-	{ "sw", "saiw", desc = "word", remap = true },
+    { "<leader>s",  group = "surround", remap = true },
+    { "<leader>sW", "saiW",             desc = "Word", remap = true },
+    { "<leader>sw", "saiw",             desc = "word", remap = true },
 })
 
 hipatterns.setup({
-	highlighters = {
-		-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
-		fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
-		hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
-		todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
-		note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+    highlighters = {
+        -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+        fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+        hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+        todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+        note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
 
-		-- Highlight hex color strings (`#rrggbb`) using that color
-		hex_color = hipatterns.gen_highlighter.hex_color(),
-	},
+        -- Highlight hex color strings (`#rrggbb`) using that color
+        hex_color = hipatterns.gen_highlighter.hex_color(),
+    },
 })
 
 pairs.setup({
-	mappings = {
-		["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
-		["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
-		["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
-		["<"] = { action = "open", pair = "<>", neigh_pattern = "[^%s\\<]." },
+    mappings = {
+        ["("] = { action = "open", pair = "()", neigh_pattern = "[^\\]." },
+        ["["] = { action = "open", pair = "[]", neigh_pattern = "[^\\]." },
+        ["{"] = { action = "open", pair = "{}", neigh_pattern = "[^\\]." },
+        ["<"] = { action = "open", pair = "<>", neigh_pattern = "[^%s\\<]." },
 
-		[")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
-		["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
-		["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
-		[">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
+        [")"] = { action = "close", pair = "()", neigh_pattern = "[^\\]." },
+        ["]"] = { action = "close", pair = "[]", neigh_pattern = "[^\\]." },
+        ["}"] = { action = "close", pair = "{}", neigh_pattern = "[^\\]." },
+        [">"] = { action = "close", pair = "<>", neigh_pattern = "[^\\]." },
 
-		['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
-		["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a<&\\].", register = { cr = false } },
-		["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^`\\].", register = { cr = false } },
-	},
+        ['"'] = { action = "closeopen", pair = '""', neigh_pattern = "[^\\].", register = { cr = false } },
+        ["'"] = { action = "closeopen", pair = "''", neigh_pattern = "[^%a<&\\].", register = { cr = false } },
+        ["`"] = { action = "closeopen", pair = "``", neigh_pattern = "[^`\\].", register = { cr = false } },
+    },
 })
