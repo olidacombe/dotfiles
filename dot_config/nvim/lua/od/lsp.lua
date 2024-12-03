1e259b000f4183a4bcc62e97cebcb976529d4bad
Oli Dacombe <olidacombe@gmail.com>
2024-12-03 22:15:04 +0000

    modernise some bits

--- dot_config/nvim/lua/od/lsp.lua
+++ dot_config/nvim/lua/od/lsp.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+M.float_diagnostics = true
+
 M.on_attach = function(client, bufnr)
     local opts = { buffer = bufnr, remap = false }
 
@@ -22,13 +24,17 @@ M.on_attach = function(client, bufnr)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
     vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1, float = true }) end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = true }) end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.jump({ count = 1, float = M.float_diagnostics }) end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.jump({ count = -1, float = M.float_diagnostics }) end, opts)
     vim.keymap.set("n", "<leader>lr", ":LspRestart<cr>", opts)
     vim.keymap.set("n", "<leader>li", ":LspInfo<cr>", opts)
     vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
     vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
     vim.keymap.set("n", "<leader>vrn", vim.lsp.buf.rename, opts)
+    vim.keymap.set("n", "<leader>vd", function()
+        M.float_diagnostics = not M.float_diagnostics
+    end, opts)
+    vim.keymap.set("n", "<leader>vi", function() vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled()) end, opts)
     vim.keymap.set("i", "<C-s>", vim.lsp.buf.signature_help, opts)
 
     local Od_Lsp = vim.api.nvim_create_augroup("Od_Lsp", {})
