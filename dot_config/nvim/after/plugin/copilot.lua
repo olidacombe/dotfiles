fa3afa89696cd4b6468d5476a59b2d05388a0f36
Oli Dacombe <olidacombe@gmail.com>
2025-03-17 21:21:13 +0000

    tame copilot mappings a bit

--- dot_config/nvim/after/plugin/copilot.lua
+++ dot_config/nvim/after/plugin/copilot.lua
@@ -2,3 +2,17 @@
 --     -- suggestion = { enabled = false },  -- disable Copilot suggestions
 --     -- panel = { enabled = false },  -- disable Copilot panel
 -- })
+
+vim.keymap.set('i', '<C-y>', 'copilot#Accept("\\<CR>")', {
+    expr = true,
+    replace_keycodes = false
+})
+vim.g.copilot_no_tab_map = true
+-- vim.keymap.set('i', '<C-g>', '<Plug>(copilot-previous)', {
+--     expr = true,
+--     noremap = false
+-- })
+-- vim.keymap.set('i', '<C-c>', '<Plug>(copilot-next)', {
+--     expr = true,
+--     noremap = false
+-- })
