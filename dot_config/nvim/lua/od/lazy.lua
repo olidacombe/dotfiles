80939c628a15b2a57a2765b9c2797c11cd0266fe
Oli Dacombe <olidacombe@gmail.com>
2025-04-05 21:58:11 +0100

    aider?

--- dot_config/nvim/lua/od/lazy.lua
+++ dot_config/nvim/lua/od/lazy.lua
@@ -110,6 +110,18 @@ local plugins = {
         dependencies = "nvim-lua/plenary.nvim",
     },
 
+    -- Aider
+    {
+        "joshuavial/aider.nvim",
+        opts = {
+            -- your configuration comes here
+            -- if you don't want to use the default settings
+            auto_manage_context = true, -- automatically manage buffer context
+            default_bindings = true,    -- use default <leader>A keybindings
+            debug = false,              -- enable debug logging
+        },
+    },
+
     -- Dressing - pretty up the inputs
     "stevearc/dressing.nvim",
 
