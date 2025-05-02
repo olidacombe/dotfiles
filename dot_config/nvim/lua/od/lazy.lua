b89c90bb3644d2de408ff160dae45933415127e4
Oli Dacombe <oliver.dacombe@gsr.io>
2025-05-02 14:43:05 +0100

    a todo that would help right now

--- dot_config/nvim/lua/od/lazy.lua
+++ dot_config/nvim/lua/od/lazy.lua
@@ -269,7 +269,7 @@ local plugins = {
 
     {
         'mrcjkb/rustaceanvim',
-        version = '^5', -- Recommended
+        version = '^6', -- Recommended
         lazy = false,   -- This plugin is already lazy
     },
 
