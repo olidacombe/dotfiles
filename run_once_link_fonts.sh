b506871786ea2c5d83caf0e312348c3b2e9f648b
Oli Dacombe <oliver.dacombe@gsr.io>
2025-07-02 10:41:50 +0100

    shrug

--- run_once_link_fonts.sh
+++ run_once_link_fonts.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 
 if [ "$(uname)" = "Darwin" ]; then
     # font repo cloned by chezmoiexternals
-    ln -sf ${HOME}/.fonts/noto-emoji/fonts/NotoEmoji-Regular.ttf ${HOME}/Library/Fonts/NotoColorEmoji-Regular.ttf
+    ln -sf ${HOME}/.fonts/noto-emoji/fonts/NotoEmoji-Regular.ttf ${HOME}/Library/Fonts/NotoColorEmoji-Regular.otf
 fi
