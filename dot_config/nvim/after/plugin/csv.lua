782200d501721482fc1b788741ca265b98d71095
Oli Dacombe <oliver.dacombe@gsr.io>
2025-12-02 12:16:43 +0000

    ❤️ csvview.nvim

---
+++ dot_config/nvim/after/plugin/csv.lua
@@ -0,0 +1,1 @@
+vim.keymap.set('n', '<leader>cv', ':CsvViewToggle<CR>', { desc = "toggle CSV view", noremap = true, silent = true })
--- dot_config/nvim/lua/od/lazy.lua
+++ dot_config/nvim/lua/od/lazy.lua
@@ -322,6 +322,29 @@ local plugins = {
     -- wilder.nvim
     { "gelguy/wilder.nvim",        build = ":UpdateRemotePlugins" },
 
+    {
+        "hat0uma/csvview.nvim",
+        ---@module "csvview"
+        ---@type CsvView.Options
+        opts = {
+            parser = { comments = { "#", "//" } },
+            keymaps = {
+                -- Text objects for selecting fields
+                textobject_field_inner = { "if", mode = { "o", "x" } },
+                textobject_field_outer = { "af", mode = { "o", "x" } },
+                -- Excel-like navigation:
+                -- Use <Tab> and <S-Tab> to move horizontally between fields.
+                -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+                -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+                -- jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+                -- jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+                -- jump_next_row = { "<Enter>", mode = { "n", "v" } },
+                -- jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+            },
+        },
+        cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+    },
+
     -- Install tools via Mason which aren't LSP servers
     "WhoIsSethDaniel/mason-tool-installer.nvim",
 
